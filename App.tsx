303647009a3e67cd51460aa3d2af6a0a860b4014
irosikoni <irosikoni@gmail.com>
2023-10-03 09:02:30 +0200

    project setup

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default function App() {
+  
+  fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=1').then(response => response.json())
+  .then(data => console.log(data));
+  
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <View></View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
